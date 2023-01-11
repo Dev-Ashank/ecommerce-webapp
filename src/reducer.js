78362fa6fce489c4de2f9af406c36398f7653e84
KUMAR DIVYASHANK GAURAV <kumardivyashankgaurav@gmail.com>
2023-01-12 03:07:35 +0530

    build context api

---
+++ src/reducer.js
@@ -0,0 +1,35 @@
+export const initialState = {
+  cart: [],
+  user: null,
+};
+
+export const getCartTotal = (cart) => {
+  return cart.reduce((amount, item) => item.price + amount, 0);
+};
+
+// Create the reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_USER":
+        return {...state,user:action.user}
+    case "Add_to_cart":
+      return { ...state, cart: [...state.cart, action.item] };
+    case "REMOVE_FROM_CART":
+      // return state.cart.filter((item) => item.title !== action.item.title);
+      //   const index = state.cart.findIndex((item) => item?.id === action.Id);
+      const index2 = state.cart.findIndex(
+        (item) => item?.title === action.title
+      );
+      let newCart = [...state.cart];
+      console.log(newCart);
+      if (index2 >= 0) {
+        newCart.splice(index2, 1);
+      } else {
+        console.warn(`product not in the cart ${action.title}`);
+      }
+      return { ...state, cart: newCart };
+    default:
+      return state;
+  }
+};
+export default reducer;
