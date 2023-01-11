682db414f77919824e50f412ba0aae7dc5c7c15a
KUMAR DIVYASHANK GAURAV <kumardivyashankgaurav@gmail.com>
2023-01-12 03:04:32 +0530

    build checkout

---
+++ src/Checkout.js
@@ -0,0 +1,38 @@
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
+import Subtotal from "./Subtotal";
+
+
+function Checkout() {
+    const [{cart,user},dispatch] = useStateValue();
+    var  str = user?.email;
+    var userName = str.split("@")
+  return (
+    <div className="checkout">
+        
+      <div className="checkout_left">
+      <h3>Hello, {userName[0]}</h3>
+        <h2 className="checkout_title">Shopping Cart</h2>
+        { cart.map(item => ( 
+            <CheckoutProduct
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+            />
+            
+        ))}
+
+      </div>
+      <div className="checkout_right">
+        < Subtotal />
+      </div>
+
+    </div>
+  );
+}
+
+export default Checkout;
---
+++ src/CheckoutProduct.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './CheckoutProduct.css'
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { useStateValue } from './StateProvider';
+
+
+function CheckoutProduct( {id,title, image, price, rating}) {
+    const [{cart},dispatch] = useStateValue();
+    
+    const removeFromCart = () => {
+       
+        // remove item from cart
+        dispatch({
+            type:'REMOVE_FROM_CART',
+            id:id,
+            title:title
+        })
+
+    }
+  return (
+    <div className='checkoutProduct'>
+         <img className='checkoutProduct_image' src={image}/>
+        <div className="checkoutProduct_info">
+            <p className="checkoutProduct_title">{title}</p>
+            <p className="checkoutProduct_price">
+                <small>₹</small> <strong>{price}</strong>
+            </p>
+
+        </div>
+
+        <div className="checkoutProduct_rating">
+            {
+                Array(rating).fill().map((_,i) =>(
+                    <p>⭐️</p>
+                ))
+            }
+        </div>
+       
+
+        <button onClick={removeFromCart}>
+            Remove from cart
+        </button>
+    </div>
+  )
+}
+
+export default CheckoutProduct
