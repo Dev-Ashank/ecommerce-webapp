53637f9619ef3935389a6d525ab3440164a099fb
KUMAR DIVYASHANK GAURAV <kumardivyashankgaurav@gmail.com>
2023-01-12 03:08:44 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-
-import './App.css';
-import Header from './Header';
-import Home from './Home';
+import "./App.css";
+import Home from "./Home";
+import Checkout from "./Checkout";
+import Header from "./Header";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from './Login'
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      console.log("User is ",authUser);
+      if(authUser)
+      {
+        dispatch({
+          type:'SET_USER',
+          user : authUser,
+
+        })
+      }
+      else{
+        dispatch({
+          type:'SET_USER',
+          user : null,
+        })
+      }
+    })
+  
+  }, [])
+  
   return (
-    <div className="App">
-      <Header/>
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        
+        <Routes>
+          <Route exact path="/" element={<><Header/><Home/></>}/>
+          <Route exact path="/login" element={<Login />}/>
+          <Route exact path="/checkout" element={<><Header/><Checkout/></>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
