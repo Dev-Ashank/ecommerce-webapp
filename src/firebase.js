a0b37eda6557a1b35df39cc18eb8b0684ad84ad3
KUMAR DIVYASHANK GAURAV <kumardivyashankgaurav@gmail.com>
2023-01-12 03:08:26 +0530

    config firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,10 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore/lite';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +17,13 @@ const firebaseConfig = {
   storageBucket: "ecommerce-webapp-7919f.appspot.com",
   messagingSenderId: "724493691640",
   appId: "1:724493691640:web:4f3ffdb84d739b87a6690d",
-  measurementId: "G-HS48SFYKR8"
+  measurementId: "G-HS48SFYKR8",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export{db,auth}
