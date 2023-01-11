e56499d43210b6f29fed6fad8c7a390c1109c552
KUMAR DIVYASHANK GAURAV <kumardivyashankgaurav@gmail.com>
2023-01-12 03:06:52 +0530

    build product

--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-function Product({id, title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
+  const [{ cart }, dispatch] = useStateValue();
+  console.log(cart);
+  const addToCart = () => {
+    dispatch({
+      type: "Add_to_cart",
+      item: {
+        id,
+        title,
+        image,
+        price,
+        rating,
+      },
+    });
+  };
   return (
     <div className="product">
-      <img src= {image} alt="" />
+      <img src={image} alt="" />
       <div className="product_info">
         <p> {title} </p>
       </div>
@@ -21,7 +36,7 @@ function Product({id, title, image, price, rating }) {
             <p>⭐️</p>
           ))}
       </div>
-      <button>Add to cart</button>
+      <button onClick={addToCart}>Add to cart</button>
     </div>
   );
 }
